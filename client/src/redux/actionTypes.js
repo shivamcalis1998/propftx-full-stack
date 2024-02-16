22df47e6adcde6dc389aaa71413459648fd45ad1
unknown <shivamswami8899@gmail.com>
2024-02-16 20:56:36 +0530

    done

--- client/src/redux/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const MOVIE_GET = "MOVIE_GET";
-export const MOVIE_POST = "MOVIE_POST";
-export const MOVIE_PUT = "MOVIE_PUT";
-export const MOVIE_DELETE = "MOVIE_DELETE";
--- client/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import storeReducer from "./reducer";
-import { thunk } from "redux-thunk";
-
-const store = createStore(storeReducer, applyMiddleware(thunk));
-
-export default store;
